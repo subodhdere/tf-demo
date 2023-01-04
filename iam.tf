4219c875ed90a854b5116dcc738bc5d82e66050a
Subodh <32792363+subodhdere@users.noreply.github.com>
2023-01-04 08:26:23 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,4 +1,9 @@
-resource "aws_iam_user" "myiamuser" {
-  name = "myiamuser"
-  path = "/system/"
+
+resource "aws_instance" "first_ec2_instance" {
+  ami = "ami-0cca134ec43cf708f"
+  instance_type = var.instance_type
+ }
+
+variable "instance_type" {
+  default = "t2.micro"
 }
