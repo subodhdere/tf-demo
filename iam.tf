faf6ae495a1bac1c671666eafcf3a16ea6fefa37
subodhdere <subodh.dere.7@gmail.com>
2023-06-20 14:59:32 +0530

    intellij

--- iam.tf
+++ iam.tf
@@ -1,9 +1,11 @@
 
 resource "aws_instance" "first_ec2_instance" {
-  ami = "ami-0cca134ec43cf708f"
+  ami           = "ami-0cca134ec43cf708f"
   instance_type = var.instance_type
- }
+}
 
 variable "instance_type" {
   default = "t2.micro"
 }
+
+##intelliJ
